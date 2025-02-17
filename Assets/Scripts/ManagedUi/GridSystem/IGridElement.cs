358e27556b1dfc771171ca28c1a10bbe6471ea5b
AchimGroner <agroner@web.de>
2025-02-17 22:30:30 +0100

    Move to submodule

--- Assets/Scripts/ManagedUi/GridSystem/IGridElement.cs
+++
@@ -1,9 +0,0 @@
-namespace ManagedUi.GridSystem
-{
-public interface IGridElement
-{
-    public int VerticalLayoutGrowth();
-    public int HorizontalLayoutGrowth();
-    public bool IgnoreLayout();
-}
-}
--- Assets/Scripts/ManagedUi/Selectables/ISelectableManager.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ManagedUi.Selectables
-{
-    public interface ISelectableManager
-    {
-        public void TriggerExternalSelect(SelectableParent selectable);
-        public void TriggerExternalDeSelect(SelectableParent selectable);
-        public void TriggerExternalConfirm(SelectableParent selectable);
-    }
-}
